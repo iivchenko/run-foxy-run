b421457561683a588aeea5352aba39ba11a8f48a
iivchenko <iivchenko@live.com>
2019-09-28 20:49:59 +0300

    Translate Foxy script from c# to f#

---
+++ game/objects/Foxy/Foxy.fs
@@ -0,0 +1,57 @@
+﻿namespace RunFoxyRun
+
+open Godot
+open System
+
+type public Foxy () =
+    inherit KinematicBody2D ()
+
+    let _up = Nullable<Vector2>(Vector2(0.0f, -1.0f))
+    let _gravity = 20.0f
+    let _maxSpeed = 200.0f
+    let _jumpHigh = -550.0f
+    let _acceleration = 50.0f
+
+    let mutable _motion = Vector2(0.0f, 0.0f)
+    let mutable _friction = false
+
+    override this._PhysicsProcess(delta:float32) = 
+        _motion.y <- _motion.y + _gravity
+        let animator = base.GetNode<AnimatedSprite>(new NodePath("Animator"))
+        
+        if Input.IsActionPressed("ui_right")
+            then
+                _motion.x <- Mathf.Min(_motion.x + _acceleration, _maxSpeed)
+                animator.FlipH <- false
+                animator.Play("run")
+            else if Input.IsActionPressed("ui_left")
+                then 
+                    _motion.x <- Mathf.Max(_motion.x - _acceleration, -_maxSpeed)
+                    animator.FlipH <- true
+                    animator.Play("run")
+            else 
+                _friction <- true
+                animator.Play("idle")
+
+        if base.IsOnFloor() 
+            then 
+                if Input.IsActionJustPressed("ui_up")
+                    then
+                        _motion.y <- _jumpHigh
+                
+                if _friction
+                    then 
+                        _motion.x <- Mathf.Lerp(_motion.x, 0.0f, 0.2f);
+            else 
+                if _motion.y < 0.0f
+                    then 
+                        animator.Play("jump")
+                    else
+                        animator.Play("fall");
+                if _friction
+                    then
+                        _motion.x <- Mathf.Lerp(_motion.x, 0.0f, 0.05f);
+                    else
+                        ()
+
+        _motion <- base.MoveAndSlide(_motion, _up)
--- game/objects/Foxy/FoxyWrap.cs
+++ game/objects/Foxy/FoxyWrap.cs
@@ -1,61 +1,6 @@
-using Godot;
-
 namespace RunFoxyRun
 {
-    public sealed class FoxyWrap : KinematicBody2D
+    public sealed class FoxyWrap : Foxy
     {
-        private Vector2 Up = new Vector2(0, -1);
-        private int Gravity = 20;
-        private int MaxSpeed = 200;
-        private int JumpHigh = -550;
-        private int Acceleration = 50;
-
-        private Vector2 _motion;
-        private bool _friction = false;
-
-        public override void _PhysicsProcess(float delta)
-        {
-            _motion.y += Gravity;
-
-            if (Input.IsActionPressed("ui_right"))
-            {
-                _motion.x = Mathf.Min(_motion.x + Acceleration, MaxSpeed);
-                GetNode<AnimatedSprite>(new NodePath("Animator")).FlipH = false;
-                GetNode<AnimatedSprite>(new NodePath("Animator")).Play("run");
-            }
-            else if (Input.IsActionPressed("ui_left"))
-            {
-                _motion.x = Mathf.Max(_motion.x - Acceleration, -MaxSpeed);
-                GetNode<AnimatedSprite>(new NodePath("Animator")).FlipH = true;
-                GetNode<AnimatedSprite>(new NodePath("Animator")).Play("run");
-            }
-            else
-            {
-                _friction = true;
-                GetNode<AnimatedSprite>(new NodePath("Animator")).Play("idle");
-            }
-
-            if (IsOnFloor())
-            {
-                if (Input.IsActionJustPressed("ui_up"))
-                {
-                    _motion.y = JumpHigh;
-                }
-
-                if (_friction)
-                    _motion.x = Mathf.Lerp(_motion.x, 0, 0.2f);
-            }
-            else
-            {
-                if (_motion.y < 0)
-                    GetNode<AnimatedSprite>(new NodePath("Animator")).Play("jump");
-                else
-                    GetNode<AnimatedSprite>(new NodePath("Animator")).Play("fall");
-                if (_friction)
-                    _motion.x = Mathf.Lerp(_motion.x, 0, 0.05f);
-            }
-
-            _motion = MoveAndSlide(_motion, Up);
-        }
     }
 }
