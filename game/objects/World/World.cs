ec2ba2acce8336010fab846c37049eb8a278a449
iivchenko <iivchenko@live.com>
2019-10-04 22:11:41 +0300

    Implement HUD with scoring; Add sound track to the World

--- game/objects/World/World.cs
+++ game/objects/World/World.cs
@@ -2,8 +2,18 @@ using Godot;
 
 public class World : Node2D
 {
+    private int _scores;
+    private Label _scoresLabel;
+
+    public override void _Ready()
+    {
+        _scores = 0;
+        _scoresLabel = GetNode<Label>(new NodePath("HUD/Scores"));
+    }
+
     public void ScorePlayer(int scores)
     {
-        GD.Print($"Score {scores}!");
+        _scores += scores;
+        _scoresLabel.Text = $"Scores: {_scores}";
     }
 }
