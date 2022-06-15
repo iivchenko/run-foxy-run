bbfd0514e8f6c2d0af7b39bf0c1e703eec13883c
iivchenko <iivchenko@live.com>
2022-06-15 12:46:19 +0200

    #30 Remove server related ps

--- infra/infra.psm1
+++ infra/infra.psm1
@@ -1,7 +1,6 @@
 ﻿$gVersion = "3.4.4"
 
 $url = "https://downloads.tuxfamily.org/godotengine/$gVersion/mono/Godot_v$gVersion-stable_mono_win64.zip"
-$serverurl="https://downloads.tuxfamily.org/godotengine/3.4.4/Godot_v3.4.4-stable_linux_headless.64.zip"
 $out = ".tools"
 
 function Download-Godot {
@@ -10,13 +9,6 @@ function Download-Godot {
     Invoke-WebRequest -Uri $url -OutFile "$out/godot.zip"
     expand-archive -path "$out/godot.zip" -DestinationPath "$out/"
     Remove-Item "$out/godot.zip"
-}
-
-function Download-SeverGodot {
-    New-Item -ItemType Directory -Force -Path $out
-    Invoke-WebRequest -Uri $serverurl -OutFile "./$out/godot.zip" -Verbose
-    expand-archive -path "./$out/godot.zip" -DestinationPath "./$out/"
-    Remove-Item "$./out/godot.zip"
 }
 
 function Run-Editor {
@@ -29,29 +21,6 @@ function Run-Editor {
     }
 
     & "$out/Godot_v$gVersion-stable_mono_win64/Godot_v$gVersion-stable_mono_win64.exe" --editor --path ./src
-}
-
-function Run-Game {
-
-    param()
-
-    if (-not (Test-Path -Path "$out/godot.exe"))
-    {
-        Download-Godot
-    }
-
-    & "$out/godot.exe"
-}
-
-function Run-ServerExport {
-     param([string]$target)
-	
-    if (-not (Test-Path -Path "$out/Godot_v$gVersion-stable_linux_headless.64"))
-    {
-        Download-SeverGodot
-    }
-
-    & "$out/Godot_v$gVersion-stable_linux_headless.64" --export $target --path ./src
 }
 
 Export-ModuleMember -Function * -Alias *
